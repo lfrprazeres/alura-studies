b6f01d9f25e53928f84e7c9ecb74c58af08f721e
Luiz Fernando <lfrprazeres@gmail.com>
2021-10-09 19:14:35 -0300

    aula6.3 - Tempo para segundos

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,8 +1,10 @@
 import Botao from "../Botao";
 import Relogio from "./Relogio";
 import style from './Cronometro.module.scss';
+import { tempoParaSegundos } from "../../common/utils/date";
 
 export default function Cronometro() {
+  console.log('conversao: ', tempoParaSegundos('01:01:01'))
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o Cronômetro</p>
