0e90e04a26c67127edbf8045e19809576ef462d5
Luiz Fernando <lfrprazeres@gmail.com>
2021-10-09 19:48:58 -0300

    aula7.4 - Finalizando a tarefa

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -14,11 +14,10 @@ export default function Item(
     id,
     selecionaTarefa
   }: Props) {
-  console.log('item atual: ', { tarefa, tempo, selecionado, completado, id })
   return (
     <li
-      className={`${style.item} ${selecionado ? style.itemSelecionado : ''}`}
-      onClick={() => selecionaTarefa(
+      className={`${style.item} ${selecionado ? style.itemSelecionado : ''} ${completado ? style.itemCompletado : ''}`}
+      onClick={() => !completado && selecionaTarefa(
         {
           tarefa,
           tempo,
@@ -30,6 +29,7 @@ export default function Item(
       >
       <h3>{tarefa}</h3>
       <span>{tempo}</span>
+      {completado && <span className={style.concluido} aria-label="tarefa completada"></span>}
     </li>
   )
 }
