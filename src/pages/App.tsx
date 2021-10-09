7afba2da6ebbce4c63ee808fc11f1eaf53eb2851
Luiz Fernando <lfrprazeres@gmail.com>
2021-10-09 12:02:44 -0300

    aula4.3 - Criando o Cronômetro

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Cronometro from '../components/Cronometro';
 import Formulario from '../components/Formulario';
 import Lista from '../components/Lista';
 import style from './App.module.scss';
@@ -8,6 +9,7 @@ function App() {
     <div className={style.AppStyle}>
       <Formulario />
       <Lista />
+      <Cronometro />
     </div>
   );
 }
