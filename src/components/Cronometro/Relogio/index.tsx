3acda6b4d7823e3cf66eb634b467d4378a5e97cc
Luiz Fernando <lfrprazeres@gmail.com>
2021-10-09 12:02:44 -0300

    aula4.3 - Criando o Cronômetro

---
+++ src/components/Cronometro/Relogio/index.tsx
@@ -0,0 +1,13 @@
+import style from './Relogio.module.scss';
+
+export default function Relogio() {
+  return (
+    <>
+      <span className={style.relogioNumero}>0</span>
+      <span className={style.relogioNumero}>0</span>
+      <span className={style.relogioDivisao}>:</span>
+      <span className={style.relogioNumero}>0</span>
+      <span className={style.relogioNumero}>0</span>
+    </>
+  )
+}
