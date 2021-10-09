fd18cbeb8d684b274edf46f8cd0ca1baf9b52546
Luiz Fernando <lfrprazeres@gmail.com>
2021-10-09 19:00:17 -0300

    aula6.2 - Selecionando uma tarefa Pt 2

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -17,7 +17,7 @@ export default function Item(
   console.log('item atual: ', { tarefa, tempo, selecionado, completado, id })
   return (
     <li
-      className={style.item}
+      className={`${style.item} ${selecionado ? style.itemSelecionado : ''}`}
       onClick={() => selecionaTarefa(
         {
           tarefa,
