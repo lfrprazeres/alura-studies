a7db7e9c17f3dc614ca44d58385441dbfe1bbb0d
Luiz Fernando <lfrprazeres@gmail.com>
2021-10-09 09:23:56 -0300

    aula2.3 - Criando a Lista

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Formulario from './components/Formulario';
+import Lista from './components/Lista';
 
 function App() {
   return (
     <div className="App">
       <Formulario />
+      <Lista />
     </div>
   );
 }
