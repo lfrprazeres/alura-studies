39f828f0563c74e797cfeff132e8efbb6a4353f8
Luiz Fernando <lfrprazeres@gmail.com>
2021-10-09 12:36:31 -0300

    aula5.2 - Controlando o formulário

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -3,9 +3,19 @@ import Botao from '../Botao';
 import style from './Formulario.module.scss';
 
 class Formulario extends React.Component {
+  state = {
+    tarefa: "",
+    tempo: "00:00"
+  }
+
+  adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
+    evento.preventDefault();
+    console.log('state: ', this.state);
+  }
+
   render() {
     return (
-      <form className={style.novaTarefa}>
+      <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
         <div className={style.inputContainer}>
           <label htmlFor="tarefa">
             Adicione um novo estudo
@@ -14,6 +24,8 @@ class Formulario extends React.Component {
             type="text"
             name="tarefa"
             id="tarefa"
+            value={this.state.tarefa}
+            onChange={evento => this.setState({ ...this.state, tarefa: evento.target.value })}
             placeholder="O que você quer estudar"
             required
           />
@@ -26,6 +38,8 @@ class Formulario extends React.Component {
             type="time"
             step="1"
             name="tempo"
+            value={this.state.tempo}
+            onChange={evento => this.setState({ ...this.state, tempo: evento.target.value })}
             id="tempo"
             min="00:00:00"
             max="01:30:00"
