5b0462a3c844b965a3016706cdd552ff48fa224c
Luiz Fernando <lfrprazeres@gmail.com>
2021-10-09 12:45:51 -0300

    aula5.3 - Adicionando uma tarefa

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cronometro from '../components/Cronometro';
 import Formulario from '../components/Formulario';
 import Lista from '../components/Lista';
+import { ITarefa } from '../types/tarefa';
 import style from './App.module.scss';
 
 function App() {
+  const [tarefas, setTarefas] = useState<ITarefa[]>([]);
   return (
     <div className={style.AppStyle}>
-      <Formulario />
-      <Lista />
+      <Formulario setTarefas={setTarefas} />
+      <Lista tarefas={tarefas} />
       <Cronometro />
     </div>
   );
