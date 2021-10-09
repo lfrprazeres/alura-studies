8fbb45c8cefb80d81312ff5d387a6f5d585dd04d
Luiz Fernando <lfrprazeres@gmail.com>
2021-10-09 11:48:51 -0300

    aula4.2 - Refatorando a Lista

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Item from './Item';
 import style from './Lista.module.scss';
 
 function Lista() {
@@ -17,10 +18,10 @@ function Lista() {
       <h2> Estudos do dia </h2>
       <ul>
         {tarefas.map((item, index) => (
-          <li key={index} className={style.item}>
-            <h3>{item.tarefa}</h3>
-            <span>{item.tempo}</span>
-          </li>
+          <Item
+            key={index}
+            {...item}
+          />
         ))}
       </ul>
     </aside>
