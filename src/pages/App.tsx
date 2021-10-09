0955bc8d67efa66d7a15fd7c195fc118b46ec79b
Luiz Fernando <lfrprazeres@gmail.com>
2021-10-09 18:55:52 -0300

    aula6.1 - Selecionando uma tarefa

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -7,10 +7,18 @@ import style from './App.module.scss';
 
 function App() {
   const [tarefas, setTarefas] = useState<ITarefa[]>([]);
+  const [selecionado, setSelecionado] = useState<ITarefa>();
+
+  function selecionaTarefa(tarefaSelecionada: ITarefa) {
+    setSelecionado(tarefaSelecionada);
+  }
   return (
     <div className={style.AppStyle}>
       <Formulario setTarefas={setTarefas} />
-      <Lista tarefas={tarefas} />
+      <Lista
+        tarefas={tarefas}
+        selecionaTarefa={selecionaTarefa}
+      />
       <Cronometro />
     </div>
   );
