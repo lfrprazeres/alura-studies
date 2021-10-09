625909746de0cbd00851632c214bb72f2047f26a
Luiz Fernando <lfrprazeres@gmail.com>
2021-10-09 10:32:10 -0300

    aula3.2 - Estilizando com Sass

---
+++ src/pages/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Formulario from '../components/Formulario';
+import Lista from '../components/Lista';
+import './style.scss';
+
+function App() {
+  return (
+    <div className="AppStyle">
+      <Formulario />
+      <Lista />
+    </div>
+  );
+}
+
+export default App;
