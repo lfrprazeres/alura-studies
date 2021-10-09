2465b8b2424b7f5c338c6fa9eab03b7fc5ad5b49
Luiz Fernando <lfrprazeres@gmail.com>
2021-10-09 09:30:38 -0300

    aula2.4 - Renderizando um array

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
 
 function Lista() {
+  const tarefas = [{
+    tarefa: 'React',
+    tempo: '02:00:00'
+  }, {
+    tarefa: 'Javascript',
+    tempo: '01:00:00'
+  }];
   return (
     <aside>
       <h2> Estudos do dia </h2>
       <ul>
-        <li>
-          <h3>
-            React
-          </h3>
-          <span>
-            02:00:00
-          </span>
-        </li>
-        <li>
-          <h3>
-            Javascript
-          </h3>
-          <span>
-            01:00:00
-          </span>
-        </li>
+        {tarefas.map((item, index) => (
+          <li key={index}>
+            <h3>{item.tarefa}</h3>
+            <span>{item.tempo}</span>
+          </li>
+        ))}
       </ul>
     </aside>
   )
