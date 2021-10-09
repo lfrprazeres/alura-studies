b116ba3c64d706a9723eed253ca40063dcb43f69
Luiz Fernando <lfrprazeres@gmail.com>
2021-10-09 19:36:59 -0300

    aula7.2 - Mostrando no relógio

--- src/components/Cronometro/Relogio/index.tsx
+++ src/components/Cronometro/Relogio/index.tsx
@@ -1,13 +1,21 @@
 import style from './Relogio.module.scss';
 
-export default function Relogio() {
+interface Props {
+  tempo: number | undefined
+}
+
+export default function Relogio({ tempo = 0 }: Props) {
+  const minutos = Math.floor(tempo / 60);
+  const segundos = tempo % 60;
+  const [minutoDezena, minutoUnidade] = String(minutos).padStart(2, '0');
+  const [segundoDezena, segundoUnidade] = String(segundos).padStart(2, '0');
   return (
     <>
-      <span className={style.relogioNumero}>0</span>
-      <span className={style.relogioNumero}>0</span>
+      <span className={style.relogioNumero}>{minutoDezena}</span>
+      <span className={style.relogioNumero}>{minutoUnidade}</span>
       <span className={style.relogioDivisao}>:</span>
-      <span className={style.relogioNumero}>0</span>
-      <span className={style.relogioNumero}>0</span>
+      <span className={style.relogioNumero}>{segundoDezena}</span>
+      <span className={style.relogioNumero}>{segundoUnidade}</span>
     </>
   )
 }
