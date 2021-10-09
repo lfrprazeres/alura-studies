747bbcba2104378b6c8532f664eaebaa41bd0abc
Luiz Fernando <lfrprazeres@gmail.com>
2021-10-09 13:58:58 -0300

    aula5.4 - Atualizando o ITarefa

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,6 +1,8 @@
+import { ITarefa } from '../../../types/tarefa';
 import style from '../Lista.module.scss';
 
-export default function Item({ tarefa, tempo }: { tarefa: string, tempo: string}) {
+export default function Item({ tarefa, tempo, selecionado, completado, id }: ITarefa) {
+  console.log('item atual: ', { tarefa, tempo, selecionado, completado, id })
   return (
     <li className={style.item}>
       <h3>{tarefa}</h3>
