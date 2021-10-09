7d88987f35195925e139b8032a3fa522c05bc29e
Luiz Fernando <lfrprazeres@gmail.com>
2021-10-09 12:45:51 -0300

    aula5.3 - Adicionando uma tarefa

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,23 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { ITarefa } from '../../types/tarefa';
 import Item from './Item';
 import style from './Lista.module.scss';
 
-function Lista() {
-  const [tarefas, setTarefas] = useState([{
-    tarefa: 'React',
-    tempo: '02:00:00'
-  }, {
-    tarefa: 'Javascript',
-    tempo: '01:00:00'
-  }, {
-    tarefa: "Typescript",
-    tempo: "03:00:00"
-  }]);
+
+function Lista({ tarefas }: { tarefas: ITarefa[] }) {
   return (
     <aside className={style.listaTarefas}>
-      <h2 onClick={() => {
-        setTarefas([...tarefas, { tarefa: "Estudar estado", tempo: "05:00:00"}])
-      }}> Estudos do dia </h2>
+      <h2> Estudos do dia </h2>
       <ul>
         {tarefas.map((item, index) => (
           <Item
