ea740e62a7cf45a41e95524a9f72a27709f087e4
Luiz Fernando <lfrprazeres@gmail.com>
2021-10-09 12:14:20 -0300

    aula5.1 - Introduzindo estados

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Item from './Item';
 import style from './Lista.module.scss';
 
 function Lista() {
-  const tarefas = [{
+  const [tarefas, setTarefas] = useState([{
     tarefa: 'React',
     tempo: '02:00:00'
   }, {
@@ -12,10 +12,12 @@ function Lista() {
   }, {
     tarefa: "Typescript",
     tempo: "03:00:00"
-  }];
+  }]);
   return (
     <aside className={style.listaTarefas}>
-      <h2> Estudos do dia </h2>
+      <h2 onClick={() => {
+        setTarefas([...tarefas, { tarefa: "Estudar estado", tempo: "05:00:00"}])
+      }}> Estudos do dia </h2>
       <ul>
         {tarefas.map((item, index) => (
           <Item
