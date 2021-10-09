d3928a72037ca800e5dad33d4b95751cc25413fa
Luiz Fernando <lfrprazeres@gmail.com>
2021-10-09 09:23:56 -0300

    aula2.3 - Criando a Lista

---
+++ src/components/Lista/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function Lista() {
+  return (
+    <aside>
+      <h2> Estudos do dia </h2>
+      <ul>
+        <li>
+          <h3>
+            React
+          </h3>
+          <span>
+            02:00:00
+          </span>
+        </li>
+        <li>
+          <h3>
+            Javascript
+          </h3>
+          <span>
+            01:00:00
+          </span>
+        </li>
+      </ul>
+    </aside>
+  )
+}
+
+export default Lista;
