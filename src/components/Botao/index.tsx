3062b90780e18709287ed840ce12672d62dbf71d
Luiz Fernando <lfrprazeres@gmail.com>
2021-10-09 10:32:10 -0300

    aula3.2 - Estilizando com Sass

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
+import './style.scss';
 
 class Botao extends React.Component {
   render() {
-    const estaAtivo = false;
-    const styles = {
-      backgroundColor: estaAtivo ? 'green' : 'red'
-    }
     return (
-      <button style={styles}>
+      <button className="botao">
         Botão
       </button>
     )
