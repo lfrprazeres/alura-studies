f0ba460e2d290d90fbad2995390902310152e8e4
Luiz Fernando <lfrprazeres@gmail.com>
2021-10-09 10:32:10 -0300

    aula3.2 - Estilizando com Sass

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Botao from '../Botao';
+import './style.scss';
 
 class Formulario extends React.Component {
   render() {
     return (
-      <form>
-        <div>
+      <form className="novaTarefa">
+        <div className="inputContainer">
           <label htmlFor="tarefa">
             Adicione um novo estudo
           </label>
@@ -17,7 +18,7 @@ class Formulario extends React.Component {
             required
           />
         </div>
-        <div>
+        <div className="inputContainer">
           <label htmlFor="tempo">
             Tempo
           </label>
