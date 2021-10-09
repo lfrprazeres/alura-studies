da4e0a358c64b521f1f72cc0f1e79b44a09705a8
Luiz Fernando <lfrprazeres@gmail.com>
2021-10-09 10:42:24 -0300

    aula3.3 - Usando CSS Modules

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './style.scss';
+import style from './Lista.module.scss';
 
 function Lista() {
   const tarefas = [{
@@ -13,11 +13,11 @@ function Lista() {
     tempo: "03:00:00"
   }];
   return (
-    <aside className="listaTarefas">
+    <aside className={style.listaTarefas}>
       <h2> Estudos do dia </h2>
       <ul>
         {tarefas.map((item, index) => (
-          <li key={index} className="item">
+          <li key={index} className={style.item}>
             <h3>{item.tarefa}</h3>
             <span>{item.tempo}</span>
           </li>
