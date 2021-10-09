151774423e6b3d8a9a34cb78694e6171f94d709f
Luiz Fernando <lfrprazeres@gmail.com>
2021-10-09 09:38:34 -0300

    aula3.1 - Aprendendo a estilizar

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 class Botao extends React.Component {
   render() {
+    const estaAtivo = false;
+    const styles = {
+      backgroundColor: estaAtivo ? 'green' : 'red'
+    }
     return (
-      <button>
+      <button style={styles}>
         Botão
       </button>
     )
