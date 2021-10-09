6ed5bda9cff373319ea0ffa695bd7f2669287107
Luiz Fernando <lfrprazeres@gmail.com>
2021-10-09 11:35:21 -0300

    aula4.1 - Conhecendo Props

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -32,7 +32,9 @@ class Formulario extends React.Component {
             required
           />
         </div>
-        <Botao />
+        <Botao>
+          Banana
+        </Botao>
       </form>
     )
   }
