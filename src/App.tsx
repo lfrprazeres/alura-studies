88442c7afcc0cb283ecf64e97b7f07ae0fea2eeb
Luiz Fernando <lfrprazeres@gmail.com>
2021-10-09 09:05:45 -0300

    aula2.1 - Criando um Componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Botao from './components/Botao';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Botao />
     </div>
   );
 }
