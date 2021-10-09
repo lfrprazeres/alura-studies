8c79265a039dc13f6d4d2dd5e12a2654626aedf4
Luiz Fernando <lfrprazeres@gmail.com>
2021-10-09 10:42:24 -0300

    aula3.3 - Usando CSS Modules

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Botao from '../Botao';
-import './style.scss';
+import style from './Formulario.module.scss';
 
 class Formulario extends React.Component {
   render() {
     return (
-      <form className="novaTarefa">
-        <div className="inputContainer">
+      <form className={style.novaTarefa}>
+        <div className={style.inputContainer}>
           <label htmlFor="tarefa">
             Adicione um novo estudo
           </label>
@@ -18,7 +18,7 @@ class Formulario extends React.Component {
             required
           />
         </div>
-        <div className="inputContainer">
+        <div className={style.inputContainer}>
           <label htmlFor="tempo">
             Tempo
           </label>
